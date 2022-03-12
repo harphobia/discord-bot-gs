c723a91d8bbca0178f404ded6104cc88267c1506
harphobia <4nh4rpn5@gmail.com>
2022-03-12 11:25:26 +0700

    disable owo for bot

--- bot.js
+++ bot.js
@@ -42,26 +42,26 @@ import moment from "moment";
     console.log(`[${moment().format("hh:mm:ss")}] Sleep 65 seconds...`);
   }, 65 * 1000);
 
-  //owo
-  setInterval(async () => {
-    const index = await randomChannelIndex(0, owoChannelList.length);
-    await sendMessage(token, owoChannelList[index], "owo pray");
-    await delay(3);
-    await sendMessage(token, owoChannelList[index], "owo sell c u r");
-    console.log(`[${moment().format("hh:mm:ss")}] Sleep 5 minutes...`);
-  }, 5 * 60 * 1000);
+  // //owo
+  // setInterval(async () => {
+  //   const index = await randomChannelIndex(0, owoChannelList.length);
+  //   await sendMessage(token, owoChannelList[index], "owo pray");
+  //   await delay(3);
+  //   await sendMessage(token, owoChannelList[index], "owo sell c u r");
+  //   console.log(`[${moment().format("hh:mm:ss")}] Sleep 5 minutes...`);
+  // }, 5 * 60 * 1000);
 
-  setInterval(async () => {
-    const index = await randomChannelIndex(0, owoChannelList.length);
-    await sendMessage(token, owoChannelList[index], "owo hunt");
-    await delay(3);
-    await sendMessage(token, owoChannelList[index], "owo coinflip 500");
-    console.log(`[${moment().format("hh:mm:ss")}] Sleep 10 seconds...`);
-  }, 10 * 1000);
+  // setInterval(async () => {
+  //   const index = await randomChannelIndex(0, owoChannelList.length);
+  //   await sendMessage(token, owoChannelList[index], "owo hunt");
+  //   await delay(3);
+  //   await sendMessage(token, owoChannelList[index], "owo coinflip 500");
+  //   console.log(`[${moment().format("hh:mm:ss")}] Sleep 10 seconds...`);
+  // }, 10 * 1000);
 
-  setInterval(async () => {
-    const index = await randomChannelIndex(0, owoChannelList.length);
-    await sendMessage(token, owoChannelList[index], "owo daily");
-    console.log(`[${moment().format("hh:mm:ss")}] Sleep 6 hours...`);
-  }, 6 * 60 * 60 * 1000);
+  // setInterval(async () => {
+  //   const index = await randomChannelIndex(0, owoChannelList.length);
+  //   await sendMessage(token, owoChannelList[index], "owo daily");
+  //   console.log(`[${moment().format("hh:mm:ss")}] Sleep 6 hours...`);
+  // }, 6 * 60 * 60 * 1000);
 })();
