627b43621dd0d410d6a346680aeb699d6bd98aff
harphobia <4nh4rpn5@gmail.com>
2022-03-14 19:43:08 +0700

    add battle

--- owo-main.js
+++ owo-main.js
@@ -23,6 +23,8 @@ import moment from "moment";
       await delay(5);
       await sendMessage(token, "952875549561339944", "owo slot 1");
       await delay(5);
+      await sendMessage(token, "952875549561339944", "owo battle");
+      await delay(5);
       await sendMessage(token, "952875549561339944", "owo coinflip 1");
       console.log(`[${moment().format("hh:mm:ss")}] Sleep 5 minutes...`);
     }, 5 * 60 * 1000);
