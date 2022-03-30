9c974c38ddd35673ad203afda43fd0acb780541d
harphobia <35424466+harphobia@users.noreply.github.com>
2022-03-31 05:21:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,24 +16,24 @@ import moment from "moment";
     setInterval(async () => {
       const index = await randomChannelIndex(0, workChannelList.length);
       await sendMessage(token, workChannelList[index], "un.crime");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "un.work");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], `un.give ${target} all`);
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "un.resetmoney");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "y");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "un.slut");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], `un.give ${target} all`);
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "un.resetmoney");
-      await delay(3);
+      await delay(5);
       await sendMessage(token, workChannelList[index], "y");
       console.log(`[${moment().format("hh:mm:ss")}] Sleep 65 seconds...`);
-    }, 65 * 1000);
+    }, 80 * 1000);
   } catch (error) {
     console.log(error);
   }
